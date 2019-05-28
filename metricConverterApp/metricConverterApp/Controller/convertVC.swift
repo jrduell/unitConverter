8761ac72526af6f476d0e007477312f5dfd7d5bc
Jacob Duell <jrduell7@gmail.com>
2019-05-27 19:41:01 -0500

    picker view and drop down menu

--- metricConverterApp/metricConverterApp/Controller/convertVC.swift
+++ metricConverterApp/metricConverterApp/Controller/convertVC.swift
@@ -8,12 +8,41 @@
 
 import UIKit
 
-class convertVC: UIViewController {
+class convertVC: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
 
+    let prefix: Array = ["nano", "micro", "milli", "centi", "deci", "unit", "deka", "hecto", "kilo", "mega", "giga", "tera"]
+    
+    @IBOutlet weak var selectBtn: CustomBtn!
+    @IBOutlet weak var unitDropDown: UIPickerView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        unitDropDown.dataSource = self
+        unitDropDown.delegate = self
+    }
+    
+    @IBAction func onSelectTapped(_ sender: Any) {
+        UIView.animate(withDuration: 0.3, animations: { self.unitDropDown.isHidden = !self.unitDropDown.isHidden })
+    }
+    
+    
+    
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return prefix.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        selectBtn.setTitle(prefix[row], for: .normal)
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return prefix[row]
     }
     
     
